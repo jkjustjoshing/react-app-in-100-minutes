633b51aa406e6e13558e294ac9d12fea15dc6676
Josh Kramer <jkjustjoshing@gmail.com>
2018-07-24 18:46:10 -0400

    Show items persisted in a Firebase firestore

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-
+import firestore from './firestore';
 class ListItems extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    firestore.collection("items").get().then((querySnapshot) => {
+      let arr = [];
+      querySnapshot.forEach(data => arr.push(data.data()));
+      this.setState({ items: arr });
+    });
+  }
 
   render() {
-    return <div>
-      Foobar
-    </div>;
+    return <ul>
+      {
+        this.state.items.map(item => <li>{item.name}, {item.number}</li>)
+      }
+    </ul>
   }
 }
 
---
+++ src/firestore.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBIxRWvYt2gI7cuqQQgUJzFPylRdZhdHWk",
+  authDomain: "react-project-f86b1.firebaseapp.com",
+  databaseURL: "https://react-project-f86b1.firebaseio.com",
+  projectId: "react-project-f86b1",
+  storageBucket: "react-project-f86b1.appspot.com",
+  messagingSenderId: "494280342591"
+});
+
+export default firebase.firestore();
