25ef1e3e7dcf76e9c7909ca8710a92765ad9fcf8
Josh Kramer <jkjustjoshing@gmail.com>
2018-07-24 18:30:10 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AddItem from './AddItem/AddItem';
+import { Route, Link } from 'react-router-dom';
+
+import AddItem from './AddItem';
+import ListItems from './ListItems';
 
 class App extends Component {
   render() {
@@ -10,12 +13,16 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
 
-        <AddItem />
+          <nav>
+            <Link to="">List Items</Link>
+            <Link to="/addItem">Add Item</Link>
+          </nav>
+        </header>
+        <section>
+          <Route exact path="" component={ListItems} />
+          <Route path="/addItem" component={AddItem} />
+        </section>
       </div>
     );
   }
